241571bf88ea13cf1a2db6f8b2e58f635cb6376b
cromeoli <cromero@iesrafaelalberti.es>
2023-05-01 14:35:42 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons:[
+          {
+          src: "/icons/logo512.png",
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+          }
+        ],
+      },
+      registerType: 'autoUpdate'
+    })],
+})
