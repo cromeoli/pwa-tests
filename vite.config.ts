16a42bc8f1f1be0b0bb2d6d742afe3ecdcf67f0f
cromeoli <cromero@iesrafaelalberti.es>
2023-05-04 18:37:36 +0200

    Cambio de icono

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [
     vue(),
     VitePWA({
       manifest: {
         icons:[
           {
-          src: "/icons/logo512.png",
+          src: "/icons/logo5.png",
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable'
@@ -18,5 +20,9 @@ export default defineConfig({
         ],
       },
       registerType: 'autoUpdate'
-    })],
+    }),
+    mkcert()
+  
+  ],
+    
 })
