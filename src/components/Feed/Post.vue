d02686a2372dec0ecac0d6b4c5b666ca71484db6
mnemonic <cromero@iesrafaelalberti.es>
2023-06-03 16:21:07 +0200

    primera versión del desplazamiento del post

--- src/components/Feed/Post.vue
+++ src/components/Feed/Post.vue
@@ -1,9 +1,61 @@
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+    data() {
+        return {
+            isDragging: false,
+            startY: 0,
+            offset: 0,
+            threshold: 400,
+        };
+    },
+    methods: {
+        handleMouseDown(event) {
+            this.isDragging = true;
+            this.startY = event.clientY;
+        },
+        handleMouseMove(event) {
+            if (this.isDragging) {
+                const deltaY = event.clientY - this.startY;
+                console.log(deltaY);
+                this.offset = deltaY / this.$refs.post.clientHeight;
+            }
+        },
+        handleMouseUp() {
+            if (this.isDragging) {
+                if (Math.abs(this.offset) >= this.threshold) {
+                    // Se superó el umbral, llamar a la función
+                    this.handleThresholdExceeded();
+                } else {
+                    // No se superó el umbral, volver a la posición inicial
+                    this.offset = 0;
+                }
+                this.isDragging = false;
+            }
+        },
+        handleThresholdExceeded() {
+            if (this.offset > 0) {
+                // Desplazamiento hacia abajo superó el umbral positivo
+                console.log('Desplazamiento hacia abajo superó el umbral');
+                // Llamar a la función correspondiente
+            } else {
+                // Desplazamiento hacia arriba superó el umbral negativo
+                console.log('Desplazamiento hacia arriba superó el umbral');
+                // Llamar a la función correspondiente
+            }
+        },
+    },
+};
 </script>
 
 <template>
-         <div class="uppBox scrollableUppbox uppBox--pink post">
+         <div class="uppBox scrollableUppbox uppBox--pink post"
+              ref="post"
+              :style="{ transform: `translateY(${offset * 100}%)` }"
+              @mousedown="handleMouseDown"
+              @mousemove="handleMouseMove"
+              @mouseup="handleMouseUp"
+              @mouseleave="handleMouseUp"
+         >
              <div class="post__iconBox">
                 <i class="material-icons">pause</i>
              </div>
