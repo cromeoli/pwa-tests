89d339835c87f86dc6b04f7c2b966cd7c9ec8e03
mnemonic <cromero@iesrafaelalberti.es>
2023-06-05 20:18:49 +0200

    Añadido hammer.js para gestionar eventos touch y ratón. Corregido un problema con la transición del menú de subir contenido.

--- src/components/Feed/Post.vue
+++ src/components/Feed/Post.vue
@@ -1,71 +1,73 @@
 <script lang="ts">
+import Hammer from 'hammerjs';
+
 export default {
     data() {
         return {
-            isDragging: false,
-            startY: 0,
             offset: 0,
-            threshold: 400,
+            threshold: 0,
+            isDragging: false,
         };
     },
+    mounted() {
+        this.setupHammer();
+    },
     methods: {
-        handleMouseDown(event) {
-            this.isDragging = true;
-            this.startY = event.clientY;
-        },
-        handleMouseMove(event) {
-            if (this.isDragging) {
-                const deltaY = event.clientY - this.startY;
-                console.log(deltaY);
-                this.offset = deltaY / this.$refs.post.clientHeight;
-            }
-        },
-        handleMouseUp() {
-            if (this.isDragging) {
-                if (Math.abs(this.offset) >= this.threshold) {
-                    // Se superó el umbral, llamar a la función
-                    this.handleThresholdExceeded();
-                } else {
-                    // No se superó el umbral, volver a la posición inicial
-                    this.offset = 0;
-                }
+        setupHammer() {
+            const hammertime = new Hammer.Manager(this.$refs.post);
+
+            hammertime.add(
+                new Hammer.Pan({
+                    threshold: this.threshold,
+                    pointers: 0,
+                    direction: Hammer.DIRECTION_VERTICAL,
+                })
+            );
+
+            hammertime.on('pan', (event) => {
+                this.offset = event.deltaY / this.$refs.post.clientHeight;
+                console.log(this.offset);
+            });
+
+            hammertime.on('panstart', (event) => {
+                this.isDragging =
+                    event.offsetDirection === Hammer.DIRECTION_DOWN ||
+                    event.offsetDirection === Hammer.DIRECTION_UP;
+            });
+
+            hammertime.on('panend', (event) => {
                 this.isDragging = false;
-            }
-        },
-        handleThresholdExceeded() {
-            if (this.offset > 0) {
-                // Desplazamiento hacia abajo superó el umbral positivo
-                console.log('Desplazamiento hacia abajo superó el umbral');
-                // Llamar a la función correspondiente
-            } else {
-                // Desplazamiento hacia arriba superó el umbral negativo
-                console.log('Desplazamiento hacia arriba superó el umbral');
-                // Llamar a la función correspondiente
-            }
+
+                if (event.deltaY > 200 || event.velocityY > 0.4) {
+                    this.offset = 1;
+                }
+                this.offset = 0;
+
+            });
         },
     },
 };
 </script>
 
 <template>
-         <div class="uppBox scrollableUppbox uppBox--pink post"
-              ref="post"
-              :style="{ transform: `translateY(${offset * 100}%)` }"
-              @mousedown="handleMouseDown"
-              @mousemove="handleMouseMove"
-              @mouseup="handleMouseUp"
-              @mouseleave="handleMouseUp"
-         >
-             <div class="post__iconBox">
-                <i class="material-icons">pause</i>
-             </div>
-             <div class="post__authorData">
-                 <div>Foto</div>
-                 <h1>Tumbalaka</h1>
-             </div>
-             <div class="post__content">
-                 La vida es una montaña rusa llena de altibajos. A veces estamos en la cima del mundo, sintiéndonos invencibles,
-                 y otras veces estamos en lo más bajo, sintiéndonos desesperados. Pero lo importante es recordar que las cosas.
-             </div>
-         </div>
+    <div
+            class="uppBox scrollableUppbox uppBox--pink post"
+            ref="post"
+            :class="{'slide-transition': isDragging}"
+            :style="{ transform: `translateY(${offset * 100}%)` }"
+    >
+        <div class="post__iconBox">
+            <i class="material-icons">pause</i>
+        </div>
+        <div class="post__authorData">
+            <div>Foto</div>
+            <h1>Tumbalaka</h1>
+        </div>
+        <div class="post__content">
+            La vida es una montaña rusa llena de altibajos. A veces estamos en la cima
+            del mundo, sintiéndonos invencibles, y otras veces estamos en lo más
+            bajo, sintiéndonos desesperados. Pero lo importante es recordar que las
+            cosas.
+        </div>
+    </div>
 </template>
