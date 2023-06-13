fbd5825c3f665d56a44b1613ccf6abefd89f1063
mnemonic <cromero@iesrafaelalberti.es>
2023-06-13 03:39:28 +0200

    El mierda post de los cojones por fin se mueve como se tiene que mover jodete mexicano

--- src/components/Feed/Post.vue
+++ src/components/Feed/Post.vue
@@ -1,75 +1,90 @@
-<script lang="ts">
-import Hammer from 'hammerjs';
-
-export default {
-    data() {
-        return {
-            offset: 0,
-            threshold: 0,
-            isDragging: false,
-        };
-    },
-    mounted() {
-        this.setupHammer();
-    },
-    methods: {
-        setupHammer() {
-
-            const postElement = this.$refs.post as HTMLElement;
-            const hammertime = new Hammer.Manager(postElement);
-
-            hammertime.add(
-                new Hammer.Pan({
-                    threshold: this.threshold,
-                    pointers: 0,
-                    direction: Hammer.DIRECTION_VERTICAL,
-                })
-            );
-
-            hammertime.on('pan', (event) => {
-                this.offset = event.deltaY / window.innerHeight;
-                console.log(this.offset);
-            });
-
-            hammertime.on('panstart', (event) => {
-                this.isDragging =
-                    event.offsetDirection === Hammer.DIRECTION_DOWN ||
-                    event.offsetDirection === Hammer.DIRECTION_UP;
-            });
-
-            hammertime.on('panend', (event) => {
-                this.isDragging = false;
-
-                if (event.deltaY > 200 || event.velocityY > 0.4) {
-                    this.offset = 1;
-                }
-                this.offset = 0;
-
-            });
-        },
-    },
-};
-</script>
-
 <template>
-        <div
-                class="uppBox scrollableUppbox uppBox--pink post"
-                ref="post"
-                :class="{'slide-transition': isDragging}"
-                :style="{ transform: `translateY(${offset * 100}%)` }"
+    <div class="post__height">
+        <div ref="card"
+             class="post__height"
+             :style="{ transform: `translateY(${offset * 100}%)` }"
         >
-            <div class="post__iconBox">
-                <i class="material-icons">pause</i>
-            </div>
-            <div class="post__authorData">
-                <div>Foto</div>
-                <h1>Tumbalaka</h1>
-            </div>
-            <div class="post__content">
-                La vida es una montaña rusa llena de altibajos. A veces estamos en la cima
-                del mundo, sintiéndonos invencibles, y otras veces estamos en lo más
-                bajo, sintiéndonos desesperados. Pero lo importante es recordar que las
-                cosas.
+            <div class="uppBox scrollableUppbox uppBox--pink post"
+            >
+                <div class="post__iconBox">
+                    <i class="material-icons">pause</i>
+                </div>
+                <div class="post__authorData">
+                    <div>Foto</div>
+                    <h1>Tumbalaka</h1>
+                </div>
+                <div class="post__content">
+                    La vida es una montaña rusa llena de altibajos. A veces estamos en la cima
+                    del mundo, sintiéndonos invencibles, y otras veces estamos en lo más
+                    bajo, sintiéndonos desesperados. Pero lo importante es recordar que las
+                    cosas.
+                </div>
             </div>
         </div>
+    </div>
 </template>
+
+<script lang="ts">
+import Hammer from 'hammerjs'
+import { ref } from '@vue/reactivity'
+import { computed, onMounted } from '@vue/runtime-core'
+
+export default {
+    setup () {
+
+        const card = ref<HTMLElement>()
+
+        const offset = ref(0)
+        const isDismissing = ref(false)
+
+        const setupHammer = () => {
+            if (!card.value) {
+                throw new Error('Missing swiper element')
+            }
+
+            const swiperManager = new Hammer.Manager(card.value as EventTarget)
+
+            swiperManager.add(new Hammer.Pan({
+                threshold: 0,
+                pointers: 0,
+                direction: Hammer.DIRECTION_VERTICAL
+            }))
+
+            swiperManager.on('pan', (e) => {
+                if (isDismissing.value) {
+                    offset.value = e.deltaY / card.value!.clientHeight
+                }
+            })
+
+            swiperManager.on('panstart', (e) => {
+                isDismissing.value = e.offsetDirection === Hammer.DIRECTION_DOWN ||
+                    e.offsetDirection === Hammer.DIRECTION_UP
+            })
+
+            swiperManager.on('panend', (e) => {
+                if (isDismissing.value) {
+                    isDismissing.value = false
+                    offset.value = 0
+
+                    if (e.deltaY > 200 || e.velocityY > 0.4) {
+                        console.log("holiiii")
+                    }
+                }
+            })
+        }
+
+        onMounted(setupHammer)
+
+        const blurCSS = computed(() => {
+            console.log("holiiii")
+        })
+
+        return {
+            card,
+            offset,
+            isDismissing,
+            blurCSS
+        }
+    }
+}
+</script>
