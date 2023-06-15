90ef7bf4f428dfe7247ec93c0f3881e85e39165c
mnemonic <cromero@iesrafaelalberti.es>
2023-06-15 18:15:17 +0200

    Establezco rutas generales a las del backend desplegado en lugar de localhost

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,8 +1,9 @@
 export const environment = {
     production: false,
-    API_URL:'http://localhost:80/api',
-    //API_URL:'https://upp.up.railway.app/api',
+    //API_URL:'http://localhost:80/api',
+    API_URL:'https://upp.up.railway.app/api',
     API_URL_IMAGE : 'http://localhost:80/api',
-    IMAGE:'http://localhost:80',
+    //IMAGE:'http://localhost:80/storage',
+    IMAGE:'https://upp.up.railway.app/storage',
     STRIPE_KEY:'',
 };
